0d165a268c49a6e82e2c3abd8cf782bdf6d86e57
MyKarken <beast964089@gmail.com>
2020-09-07 10:45:49 +0800

    Updates

---
+++ precache-manifest.2e07d81a788192d2ca85e1eae0c420d6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a541ddf6128e9e80ee648db92b91de5",
+    "url": "/React-ERP-Demo/index.html"
+  },
+  {
+    "revision": "6fd8b77b94bc3addc715",
+    "url": "/React-ERP-Demo/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "772cc1d784b9f0447958",
+    "url": "/React-ERP-Demo/static/css/main.0f01a030.chunk.css"
+  },
+  {
+    "revision": "6fd8b77b94bc3addc715",
+    "url": "/React-ERP-Demo/static/js/2.a23f277f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-ERP-Demo/static/js/2.a23f277f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "772cc1d784b9f0447958",
+    "url": "/React-ERP-Demo/static/js/main.887bb196.chunk.js"
+  },
+  {
+    "revision": "202c9100c692e2de8d47",
+    "url": "/React-ERP-Demo/static/js/runtime-main.2644438e.js"
+  },
+  {
+    "revision": "05470872fe850a2eb4a0dfbce9e9b900",
+    "url": "/React-ERP-Demo/static/media/rbg.05470872.png"
+  },
+  {
+    "revision": "a1bb1ef831accf370d5f7d35add14d06",
+    "url": "/React-ERP-Demo/static/media/react.a1bb1ef8.jpg"
+  },
+  {
+    "revision": "61608a4068c81e43f40f017490725775",
+    "url": "/React-ERP-Demo/static/media/reactBg.61608a40.jpg"
+  },
+  {
+    "revision": "d6759143baffd6711527ab1dcffcf093",
+    "url": "/React-ERP-Demo/static/media/user-logo.d6759143.png"
+  }
+]);
